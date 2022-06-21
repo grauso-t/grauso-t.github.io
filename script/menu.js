92ead3a227af289631857393e001d692f69a2588
grauso-t <grauso.teo@gmail.com>
2022-06-21 23:28:18 +0200

    WebSite

---
+++ script/menu.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $("nav#navbar").load("/resources/navbar.html");
+    $("footer#footer").load("/resources/footer.html");
+});
+
+window.onresize = function(event) {
+    if (document.documentElement.clientWidth > 900)
+        $("div#navbar-menu").show();
+    else
+        $("div#navbar-menu").hide();
+};
+
+function showMenu() {
+    $("div#navbar-menu").slideToggle();
+}
