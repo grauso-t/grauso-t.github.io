1ca19a6d7411ed81fd5e5645234f167008f1c101
grauso-t <grauso.teo@gmail.com>
2022-07-27 19:47:19 +0200

    Modifiche CSS

--- script/menu.js
+++ script/menu.js
@@ -1,10 +1,22 @@
 $(document).ready(function() {
-    $("nav#navbar").load("/resources/navbar.html");
+    $("nav#navbar").load("/resources/navbar.html", function() {
+
+        let currentPage = document.title;
+    
+        $(".navbar-item").each(function() {
+    
+            if (currentPage === (this.getAttribute("data-item-type"))) {
+                $(this).closest("div").addClass("currentPage");
+            }
+        });
+    });
+
     $("footer#footer").load("/resources/footer.html");
 });
 
 window.onresize = function(event) {
-    if (document.documentElement.clientWidth > 900)
+
+    if (document.documentElement.clientWidth > 600)
         $("div#navbar-menu").show();
     else
         $("div#navbar-menu").hide();
